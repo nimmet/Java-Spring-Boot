6d46e9a0a7aaa725a44f551d3c1438395a8fef96
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-12 18:18:13 +0200

    Demo Spring project -  Create Spring project and add some dependencies and properites

---
+++ 01-spring-boott-overview/01-spring-boot-demo/src/main/java/com/uyghur/mycoolappdemo/controller/MyAppController.java
@@ -0,0 +1,32 @@
+package com.uyghur.mycoolappdemo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyAppController {
+
+    private String description = """
+                Hallo Liebe Leute,\n
+                Ich bin ein Uyghur und ich komme aus Easten Turkistan.\n
+                Ich habe Computer Engineering studiert und arbeite bei einem Firma \n
+                als Cybersecurity Specialist.
+                """;
+
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Uyghur!";
+    }
+
+    @GetMapping("/uyghur")
+    public String showMsg(){
+        return description;
+    }
+
+    @GetMapping("/description")
+    public String showDescription(){
+        return "description";
+    }
+}
