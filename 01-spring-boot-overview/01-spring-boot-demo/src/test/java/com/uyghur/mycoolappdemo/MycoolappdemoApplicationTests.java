f0749eb2268edd197ae3991689209473d81a46b3
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-12 18:57:40 +0200

    Spring - devtools demo

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/test/java/com/uyghur/mycoolappdemo/MycoolappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyghur.mycoolappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
