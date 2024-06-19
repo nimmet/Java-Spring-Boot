46108011e995d8d4599f34a094a579acef0358a0
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-19 17:23:46 +0200

    Thymeleaf demo project

---
+++ 07-spring-boot-spring-mvc-thymeleaf-demo/00-spring-boot-spring-mvc-thymeleafdemo/src/main/java/com/uyghur/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.uyghur.springboot.thymeleafdemo.dao;
+
+import com.uyghur.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+}
