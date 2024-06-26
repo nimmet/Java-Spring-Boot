eaff6b95cb204d1cf056e94c1136155f840d85c2
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-26 17:34:48 +0200

    Spring-boot-aop-project

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/test/java/com/uyghur/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyghur.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
